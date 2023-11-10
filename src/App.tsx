00060efecfd0819a1a0e2ed66d149ae54a8effae
Brian Perkins <bperkins06@users.noreply.github.com>
2023-11-10 16:49:19 -0600

    complete and remove todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+
 type Todo = {
   id: number;
   task: string;
@@ -11,31 +12,37 @@ function App() {
   const [task, setTask] = useState<string>("");
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setTask(event.target.value);
+    setTask(event.target.value);
   };
 
   const handleFormSubmit = (event: React.FormEvent) => {
-      event.preventDefault();
+    event.preventDefault();
 
-     if (task.trim().length === 0) {
-          alert("Please enter a value!");
-          return;
-      }
+    if (task.trim().length === 0) {
+      alert("Please enter a value!");
+      return;
+    }
 
-      const todo: Todo = {
-          id: Date.now(),
-          task: task,
-          isCompleted: false,
-      };
+    const todo: Todo = {
+      id: Date.now(),
+      task: task,
+      isCompleted: false,
+    };
 
-      setTodos([todo, ...todos]);
-      setTask("");
+    setTodos([todo, ...todos]);
+    setTask("");
   };
 
   const handleChangeChecked = (todo: Todo) => {
     const index = todos.indexOf(todo);
     todo.isCompleted = !todo.isCompleted;
     todos.splice(index, 1, todo);
+    setTodos([...todos]);
+  };
+
+  const handleDelete = (id: number) => {
+    const index = todos.findIndex((todo) => todo.id === id);
+    todos.splice(index, 1);
     setTodos([...todos]);
   };
 
@@ -49,13 +56,12 @@ function App() {
         {todos.map((todo) => (
           <li key={todo.id}>
             {todo.task}
-            {/*
             <input
                 type="checkbox"
                 checked={todo.isCompleted}
                 onChange={() => handleChangeChecked(todo)}
             />
-            */}
+            <button onClick={() => handleDelete(todo.id)}>Remove</button>
           </li>
         ))}
       </ul>
