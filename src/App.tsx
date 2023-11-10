9f77a7cb5ab83c9f8d6dd43d1941be0f9bb2049f
Brian Perkins <bperkins06@users.noreply.github.com>
2023-11-10 17:54:39 -0600

    input autofocus onload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+
+// ----------------------------------------------
+// Start with https://medium.com/@dhanushkatharindu99/simple-todo-app-with-react-and-typescript-77da1189dc18
+// Then use https://www.freecodecamp.org/news/typescript-tutorial-for-react-developers/
+//
 
 type Todo = {
   id: number;
@@ -10,6 +15,13 @@ type Todo = {
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [task, setTask] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTask(event.target.value);
@@ -49,7 +61,12 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={handleFormSubmit}>
-          <input type="text" name="task" onChange={handleInput} />
+          <input
+            ref={inputRef}
+            type="text"
+            name="task"
+            onChange={handleInput}
+          />
           <button type="submit">Submit</button>
       </form>
       <ul>
