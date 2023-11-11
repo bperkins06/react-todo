393c0197df09aef20dff499b38c5f3b94d4e402d
Brian Perkins <bperkins06@users.noreply.github.com>
2023-11-10 19:51:39 -0600

    added joke get API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 // ----------------------------------------------
 // Start with https://medium.com/@dhanushkatharindu99/simple-todo-app-with-react-and-typescript-77da1189dc18
 // Then use https://www.freecodecamp.org/news/typescript-tutorial-for-react-developers/
+// APIs: https://builtin.com/software-engineering-perspectives/react-api
 //
 
 type Todo = {
@@ -16,12 +18,30 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [task, setTask] = useState<string>("");
   const inputRef = useRef<HTMLInputElement>(null);
+  const [joke, setJoke] = useState<string>("");
 
   useEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus()
     }
   }, [])
+
+  const getJoke = (event: React.MouseEvent<HTMLButtonElement>) => {
+    Axios.get("https://official-joke-api.appspot.com/random_joke").then(
+      (response) => {
+        console.log(response);
+        setJoke(response.data.setup+"..."+response.data.punchline);
+      }
+    );
+  };
+
+  const fetchJoke = (event: React.MouseEvent<HTMLButtonElement>) => {
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then((response) => response.json())
+      .then((data) => {
+        setJoke(data.setup+"..."+data.punchline);
+      });
+  };
 
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTask(event.target.value);
@@ -60,28 +80,35 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={handleFormSubmit}>
-          <input
-            ref={inputRef}
-            type="text"
-            name="task"
-            onChange={handleInput}
-          />
-          <button type="submit">Submit</button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.task}
+      <div className="App-jokes">
+        <button className="App-button" onClick={getJoke}>Axios Joke?</button>
+        <button className="App-button" onClick={fetchJoke}>Fetch Joke?</button>
+        <div>{joke}</div>
+      </div>
+      <div className="App-todos">
+        <form onSubmit={handleFormSubmit}>
             <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={() => handleChangeChecked(todo)}
+              ref={inputRef}
+              type="text"
+              name="task"
+              onChange={handleInput}
             />
-            <button onClick={() => handleDelete(todo.id)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+            <button type="submit">Submit</button>
+        </form>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.task}
+              <input
+                  type="checkbox"
+                  checked={todo.isCompleted}
+                  onChange={() => handleChangeChecked(todo)}
+              />
+              <button onClick={() => handleDelete(todo.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
